eb36c56d7bc41d5ea0662d7aa454e464e9fa85e9
cremor <cremor@users.noreply.github.com>
2023-06-06 08:28:16 +0200

    Add sample

---
+++ CodeFixProviderErrorSample/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeFixProviderErrorSample
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Func<int, int> test = (int n) => n * 2;
+        }
+    }
+}
